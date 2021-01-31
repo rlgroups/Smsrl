c35c5bb501d513bff3b33fc6e293ce1ac6acf4b1
yehudafh <yehudafh@gmail.com>
2021-01-31 11:43:20 +0200

    Update config.php

--- src/Config/config.php
+++ src/Config/config.php
@@ -1,12 +1,12 @@
 <?php
 
 return [
-    'server'    => env('SMSRL_SERVER', 'http://80.179.185.164:8081/SMS/SendMassage.php?'),
+    'server'    => env('SMSRL_SERVER', ''),
     'user'    => env('SMSRL_KEY', ''),
     'password' => env('SMSRL_SECRET', ''),
     'curl' => [
         // 'curl' => [CURLOPT_SSL_VERIFYPEER => false],
         // 'verify' => false,
     ],
-    'test_phone' => env('SMSRL_TEST_PHONE', '0502081560,0556667777'),
+    'test_phone' => env('SMSRL_TEST_PHONE', ''),
 ];
